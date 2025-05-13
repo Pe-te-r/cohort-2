3a2f6201100cb8c4e7a3cde35a05bbdd1620ef8b
Pe-te-r <mburupeter8526@gmail.com>
2025-05-13 20:45:54 +0300

    more

--- js/index.ts
+++ js/index.ts
@@ -1,3 +1,5 @@
+// import fs from 'fs'
+
 let userName: string = 'peter'
 
 class Person{
@@ -14,9 +16,17 @@ class Person{
     showInfo() {
         console.log(`Am ${this.firstName} ${this.lastName} of age ${this.age}`)
     }
+    get ageInfo(){
+        return this.age
+    }
+    set ageInfo(age:number) {
+        this.age=age
+    }
 }
 
 const person = new Person('peter','wahu',22)
 const person2 = new Person('shakirah','muthoni',21)
 person.showInfo()
 person2.showInfo()
+person.ageInfo=30
+console.log(person.ageInfo)
